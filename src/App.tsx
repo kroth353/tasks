b1eabf64abf16e65d97ba7dedbf40cebb9d9ef30
Kayla <kjroth@udel.edu>
2023-02-13 16:09:51 -0500

    task 3 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
+import quokka from "./quokka.jpeg";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +14,51 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <header> Beginning of Task 3 </header>
+            <p></p>
+
+            <header></header>
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>{" "}
+                        <p></p>
+                        Fruits:
+                        <ol>
+                            <li>Bananas </li>
+                            <li>Apples</li>
+                            <li>Oranges</li>
+                            <div
+                                style={{
+                                    backgroundColor: "red",
+                                    width: 150,
+                                    height: 50
+                                }}
+                            >
+                                {" This will have a red background"}
+                            </div>
+                        </ol>
+                    </Col>
+
+                    <Col>
+                        <p>
+                            <img src={quokka} alt="Quokka" />
+                        </p>
+                        <div
+                            style={{
+                                backgroundColor: "red",
+                                width: 150,
+                                height: 50
+                            }}
+                        >
+                            {" This will also have a red background"}
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
